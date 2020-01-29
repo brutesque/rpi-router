c2094b25afb79b03c8773d0f6deadbb531d10671
Brutesque <brutesque@users.noreply.github.com>
2020-01-29 15:16:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,22 @@ sudo apt install --yes git
 
 sudo rm -Rf /tmp/rpi-cellular-router
 sudo git clone https://github.com/brutesque/rpi-cellular-router /tmp/rpi-cellular-router
+
+# https://www.g7smy.co.uk/2014/08/raspberry-pi-as-a-3g4g-router/
+
+# Configuring the network
+sudo cp /tmp/rpi-cellular-router/etc/sysctl.conf /etc/sysctl.conf
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo cp /tmp/rpi-cellular-router/etc/network/interfaces /etc/network/interfaces
+
+# Setup DHCP
+sudo apt install --yes isc-dhcp-server
+sudo cp /tmp/rpi-cellular-router/etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf
+
+# Accessing the internet
+sudo apt install --yes iptables
+chmod +x /tmp/rpi-cellular-router/ipt.sh
+sudo /tmp/rpi-cellular-router/ipt.sh
+sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
+sudo cp /tmp/rpi-cellular-router/etc/network/if-up.d/iptables /etc/network/if-up.d/iptables
+sudo chmod +x /etc/network/if-up.d/iptables
