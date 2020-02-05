21243a6c7ff7552d8ef6553ff901628647682924
Brutesque <brutesque@users.noreply.github.com>
2020-02-05 17:36:12 +0100

    update

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
-# Raspberry Pi Cellular Router
+# Raspberry Pi Router
 
-These instructions will help create a router from a raspberry pi, to connect a local network to the internet through a
-LTE USB Stick.
+These instructions will help create a router from a raspberry pi. The goal is to setup eth0 as LAN and eth1 as WAN.
+This particular setup works with a raspberry pi B2 and a Huawei E3372 LTE USB Stick. 
 
-Tested with the Huawei E3372.
+Write 2019-09-26-raspbian-buster-lite.img to a SD-card. Insert the SD-card into the raspberry pi and start it up.
+Log into the Raspberry Pi and check that you can connect to the internet:
+```bash
+ping 1.1.1.1
+```
 
+Enter the following command to setup the router:
+```bash
+bash -c "$(curl -fsSL raw.githubusercontent.com/brutesque/rpi-router/master/install.sh)"
+```
+
+
+
+Resources:
+https://www.g7smy.co.uk/2014/08/raspberry-pi-as-a-3g4g-router/
+https://www.fastnetserv.com/how-to-make-a-raspberry-pi-3-as-a-3g4g-router/
--- install.sh
+++ install.sh
@@ -5,24 +5,24 @@ set -e
 sudo apt update
 sudo apt install --yes git
 
-sudo rm -Rf /tmp/rpi-cellular-router
-sudo git clone https://github.com/brutesque/rpi-cellular-router /tmp/rpi-cellular-router
+sudo rm -Rf /tmp/rpi-router
+sudo git clone https://github.com/brutesque/rpi-router /tmp/rpi-router
 
 # Configuring the network
-sudo cp /tmp/rpi-cellular-router/etc/sysctl.conf /etc/sysctl.conf
+sudo cp /tmp/rpi-router/etc/sysctl.conf /etc/sysctl.conf
 sudo sysctl -w net.ipv4.ip_forward=1
 
-sudo cp /tmp/rpi-cellular-router/etc/network/interfaces /etc/network/interfaces
+sudo cp /tmp/rpi-router/etc/network/interfaces /etc/network/interfaces
 
 # Setup DHCP
 sudo apt install --yes isc-dhcp-server
-sudo cp /tmp/rpi-cellular-router/etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf
-sudo cp /tmp/rpi-cellular-router/etc/default/isc-dhcp-server /etc/default/isc-dhcp-server
+sudo cp /tmp/rpi-router/etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf
+sudo cp /tmp/rpi-router/etc/default/isc-dhcp-server /etc/default/isc-dhcp-server
 
 # Accessing the internet
 sudo apt install --yes iptables
-sudo chmod +x /tmp/rpi-cellular-router/ipt.sh
-sudo /tmp/rpi-cellular-router/ipt.sh
+sudo chmod +x /tmp/rpi-router/ipt.sh
+sudo /tmp/rpi-router/ipt.sh
 sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
-sudo cp /tmp/rpi-cellular-router/etc/network/if-up.d/iptables /etc/network/if-up.d/iptables
+sudo cp /tmp/rpi-router/etc/network/if-up.d/iptables /etc/network/if-up.d/iptables
 sudo chmod +x /etc/network/if-up.d/iptables
